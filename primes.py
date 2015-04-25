fcf23eff1f02effd066809fc986c87a3cce67fa7
Laura Elena Pérez Calderón <LauraElenaPC@users.noreply.github.com>
2015-04-25 14:04:17 -0500

    update
    añdi el codigo

--- primes.py
+++ primes.py
@@ -1,1 +1,11 @@
-
+class prime_class:
+  x=int(input("Ingrese un Numero: "))
+  def is_prime():
+    if x<2:
+      return False
+      for i in range(2,x):
+        if x%i == 0:
+          return False
+          else:
+            return True
+                
